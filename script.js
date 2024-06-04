76a00aced25f60dd02c486a56f8aabe0f160974a
satish nagraj <satishnagraj54@gamil.com>
2024-06-04 15:57:15 +0530

    add functionality

--- script.js
+++ script.js
@@ -15,8 +15,8 @@ async function fetchRecipes(inp) {
     return res.data;
 
   }
-  catch (e) {
-    return "Data not Found", (e);
+  catch (error) {
+     recipeContainer.innerHTML = "<H2> ERROE IN FETCHING RECIPES .............</H2>"
   }
 
 }
@@ -38,43 +38,55 @@ function show(recipes) {
     //add Event listene
 
     btn2.addEventListener("click", () => {
-     openRecipePopup(recipe); 
+      openRecipePopup(recipe);
 
     })
     recipeContainer.appendChild(recipeDev);
   });
 }
 
-function  openRecipePopup(recipe){
-             
-  recipeDetailsContant.innerHTML = ` <h2>${recipe.strMeal}</h2>
+function openRecipePopup(recipe) {
+
+  recipeDetailsContant.innerHTML = ` <h2 class="recipeName">${recipe.strMeal}</h2>
   
     <h3>Ingredents:</h3>
-    <ul>${fetchIngredents(recipe)}</ul>
-  `           
-             
-      recipeDetailsContant.parentElement.style.display = "block ";       
-} 
+    <ul  class="IngredentsList">${fetchIngredents(recipe)}</ul>
+    <div class="instructions">
+    <h3 class ="h3">Instructions:</h3>
+    <p>${recipe.strInstructions}</p>
+    </div>
+  `
+
+  recipeDetailsContant.parentElement.style.display = "block ";
+}
 // function to fetcing ingredents===============================================================
-function fetchIngredents(recipe){
-      let ingredentsList = " ";
-      for(let i = 1; i<=20;i++){
-        const ingredent = recipe[`strIngredient${i}`];
-        if(ingredent){
-          const measure  = recipe[`strMeasure${i}`];
-          ingredentsList += `<li>${measure} ${ingredent}</li>`
-        }
-        else{
-          break;
-        }
-      }
-      return ingredentsList;
+function fetchIngredents(recipe) {
+  let ingredentsList = " ";
+  for (let i = 1; i <= 20; i++) {
+    const ingredent = recipe[`strIngredient${i}`];
+    if (ingredent) {
+      const measure = recipe[`strMeasure${i}`];
+      ingredentsList += `<li>${measure} ${ingredent}</li>`
+    }
+    else {
+      break;
+    }
+  }
+  return ingredentsList;
 }
 
+closeButton.addEventListener("click", () => {
+  recipeDetailsContant.parentElement.style.display = "none";
+});
 btn.addEventListener("click", async (e) => {
   e.preventDefault(e);
   let inp = search.value.trim();
+  if (inp == "") {
+    recipeContainer.innerHTML = `<h2>Type the meal what you want.....</h2>`
+    return;
+  }
   let recipes = await fetchRecipes(inp)
-  console.log(recipes);
+
+  // console.log(recipes);
   show(recipes);
 })
