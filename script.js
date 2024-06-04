3fc29b1bbf0b916cbbfad991c4ec58197a39dd7a
satish nagraj <satishnagraj54@gamil.com>
2024-06-04 13:26:10 +0530

    add style

--- script.js
+++ script.js
@@ -47,10 +47,29 @@ function show(recipes) {
 
 function  openRecipePopup(recipe){
              
-  recipeDetailsContant.innerHTML = ` <h2>${recipe.strMeal}</h2> `           
+  recipeDetailsContant.innerHTML = ` <h2>${recipe.strMeal}</h2>
+  
+    <h3>Ingredents:</h3>
+    <ul>${fetchIngredents(recipe)}</ul>
+  `           
              
       recipeDetailsContant.parentElement.style.display = "block ";       
 } 
+// function to fetcing ingredents===============================================================
+function fetchIngredents(recipe){
+      let ingredentsList = " ";
+      for(let i = 1; i<=20;i++){
+        const ingredent = recipe[`strIngredient${i}`];
+        if(ingredent){
+          const measure  = recipe[`strMeasure${i}`];
+          ingredentsList += `<li>${measure} ${ingredent}</li>`
+        }
+        else{
+          break;
+        }
+      }
+      return ingredentsList;
+}
 
 btn.addEventListener("click", async (e) => {
   e.preventDefault(e);
